342163b358dc394a7640afd562700dc2867d16bb
azuyes <36449201+azuyes@users.noreply.github.com>
2024-08-30 17:52:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 set -e
 
 # 启动数据库
-python /app/app/backend_pre_start.py
+#python /app/app/backend_pre_start.py
 # 运行数据库迁移
-alembic upgrade head
+#alembic upgrade head
 # 初始化数据库
-python /app/app/initial_data.py
+#python /app/app/initial_data.py
 # 启动应用
-exec uvicorn app.main:app --reload --host 0.0.0.0 --port 80
+#exec uvicorn app.main:app --reload --host 0.0.0.0 --port 80
+python /app/app/main.py
